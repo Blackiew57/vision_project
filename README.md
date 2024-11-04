8bd7c6cc9e7527526bf5e54b41d06f0876a5c10e
Blackiew57 <hsp070520@gmail.com>
2024-11-04 22:10:47 +0900

    modified:   .gitignore 	modified:   README.md 	new file:   filter.py 	new file:   panorama.py 	new file:   pyproject.toml 	new file:   requirements.txt

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# vision_project
+# Computer Vision Project 
+
+This project was developed as part of a Computer Vision class at SahmYook University. The project includes a implementation of panorama stitching and image filtering using the OpenCV library.
+
+## Features
+
+- **Panorama Creation**: Combine multiple images to create a single panoramic view.
+- **Image Filtering**: Apply various filters to images for different effects.
+
+## Technologies Used
+
+- **Python** : 3.10 or higher
+- **OpenCV**: For image processing functions.
+- **PyInstaller**: Package the project into an executable file (.exe) for Windows.
+- **Custom Icons**: The .exe file icon was customized with Dogecoin and Shiba Inu coin icons.
+- **For more** : please read requirements.txt
+
+## Running the Project
+
+The project has been compiled into a Windows executable file, which can be run without needing Python installed. Although you can't download through my github, you can create your own `.exe` file using Pyinstaller. To make your own `.exe` open Terminal and type `pyinstaller -w -F panorama.py` or `pyinstaller -w -F filter.py`. You can also add your own icon.
---
+++ filter.py
@@ -0,0 +1,104 @@
+from PyQt6.QtWidgets import *
+
+import cv2 as cv
+import numpy as np
+import sys
+
+
+class SpecialEffect(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('사진 특수 효과')
+        self.setGeometry(1000,1000,1000,200)
+        
+        pictureButton = QPushButton('사진 읽기', self)
+        embossButton = QPushButton('엠보싱', self)
+        cartoonButton = QPushButton('카툰', self)
+        sketchButton = QPushButton('연필 스케치',self)
+        oilButton = QPushButton('유화', self)
+        CannyButton = QPushButton('캐니엣지', self)
+        BlurButton = QPushButton('블러',self)
+        saveButton = QPushButton('저장하기', self)
+        self.pickCombo = QComboBox(self)
+        self.pickCombo.addItems(['엠보싱', '카툰', '연필 스케치(명암)', '연필 스케치(컬러)', '유화', '캐니엣지','블러'])
+        quitButton = QPushButton('나가기', self)
+        self.label = QLabel('환영합니다!', self)
+        
+        pictureButton.setGeometry(10,10,100,30)
+        embossButton.setGeometry(110,10,100,30)
+        cartoonButton.setGeometry(210,10,100,30)
+        sketchButton.setGeometry(310,10,100,30)
+        oilButton.setGeometry(410,10,100,30)
+        CannyButton.setGeometry(510,10,100,30)
+        BlurButton.setGeometry(610,10,100,30)
+        saveButton.setGeometry(710,10,100,30)
+        self.pickCombo.setGeometry(710,40,110,30)
+        quitButton.setGeometry(820,10,100,30)
+        self.label.setGeometry(10,40,500,170)
+        
+        pictureButton.clicked.connect(self.pictureOpenFunction)
+        embossButton.clicked.connect(self.embossFunction)
+        cartoonButton.clicked.connect(self.cartoonFunction)
+        sketchButton.clicked.connect(self.sketchFunction)
+        oilButton.clicked.connect(self.oilFunction)
+        CannyButton.clicked.connect(self.CannyFunction)
+        BlurButton.clicked.connect(self.BlurFunction)
+        saveButton.clicked.connect(self.saveFunction)
+        quitButton.clicked.connect(self.quitFunction)
+        
+    def pictureOpenFunction(self):
+        fname = QFileDialog.getOpenFileName(self,'사진 읽기','./')
+        self.img = cv.imread(fname[0])
+        if self.img is None: sys.exit('파일을 찾을 수 없습니다.')
+        
+        cv.imshow('Painting',self.img)
+        
+    def embossFunction(self):
+        femboss = np.array([[-1.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 1.0]])
+        
+        gray = cv.cvtColor(self.img, cv.COLOR_BGR2GRAY)
+        gray16 = np.int16(gray)
+        self.emboss = np.uint8(np.clip(cv.filter2D(gray16,-1,femboss)+128, 0, 255))
+        
+        cv.imshow('Emboss', self.emboss)
+        
+    def cartoonFunction(self):
+        self.cartoon = cv.stylization(self.img, sigma_s=60, sigma_r=0.45)
+        cv.imshow('Cartoon', self.cartoon)
+        
+    def sketchFunction(self):
+        self.sketch_gray,self.sketch_color = cv.pencilSketch(self.img, sigma_s=60, sigma_r=0.07, shade_factor=0.02)
+        cv.imshow('Pencil sketch(gray)', self.sketch_gray)
+        cv.imshow('Pencil sketch(color)', self.sketch_color)
+        
+    def oilFunction(self):
+        self.oil = cv.xphoto.oilPainting(self.img, 10, 1, cv.COLOR_BGR2Lab)
+        cv.imshow('Oil painting', self.oil)
+    
+    def CannyFunction(self):
+        self.canny = cv.Canny(self.img, 100, 200)
+        cv.imshow('Canny Edge', self.canny)
+    
+    def BlurFunction(self):
+        self.blur = cv.GaussianBlur(self.img, (9,9), 0, 0)
+        cv.imshow('Gaussian Blur', self.blur)
+        
+    def saveFunction(self):
+        fname = QFileDialog.getSaveFileName(self, '파일저장', './')
+        
+        i = self.pickCombo.currentIndex()
+        if i == 0: cv.imwrite(fname[0], self.emboss)
+        elif i == 1: cv.imwirte(fname[0], self.cartoon)
+        elif i == 2: cv.imwrite(fname[0], self.sketch_gray)
+        elif i == 3: cv.imwrite(fname[0], self.sketch_color)
+        elif i == 4: cv.imwrite(fname[0], self.oil)
+        
+    def quitFunction(self):
+        cv.destroyAllWindows()
+        self.close()
+        
+app = QApplication(sys.argv)    
+win = SpecialEffect()
+win.show()
+app.exec()
+        
